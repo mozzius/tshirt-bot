f67705d77f3d9cbe98fa58eb23e03c18c2d7fee6
Samuel Newman <mozzius@protonmail.com>
2024-02-15 01:13:51 +0000

    compress to fuck

--- src/image.ts
+++ src/image.ts
@@ -14,28 +14,30 @@ export const createImage = async (src: string) => {
       channels: 3,
       background: { r: 255, g: 255, b: 255 },
     },
-  }).composite([
-    { input: await sharp(tshirt).resize(650, 650).toBuffer() },
-    {
-      input: await sharp(design)
-        .resize(650 * 0.5)
-        .toBuffer(),
-    },
-    {
-      input: await sharp(logo)
-        .resize(800)
-        .rotate(-15, { background: { r: 0, g: 0, b: 0, alpha: 0 } })
-        .toBuffer(),
-      top: -20,
-      left: -20,
-    },
-    {
-      input: await sharp(buy)
-        .resize(400)
-        .rotate(5, { background: { r: 0, g: 0, b: 0, alpha: 0 } })
-        .toBuffer(),
-      top: 450,
-      left: 700,
-    },
-  ]);
+  })
+    .composite([
+      { input: await sharp(tshirt).resize(650, 650).toBuffer() },
+      {
+        input: await sharp(design)
+          .resize(650 * 0.5)
+          .toBuffer(),
+      },
+      {
+        input: await sharp(logo)
+          .resize(800)
+          .rotate(-15, { background: { r: 0, g: 0, b: 0, alpha: 0 } })
+          .toBuffer(),
+        top: -20,
+        left: -20,
+      },
+      {
+        input: await sharp(buy)
+          .resize(400)
+          .rotate(5, { background: { r: 0, g: 0, b: 0, alpha: 0 } })
+          .toBuffer(),
+        top: 450,
+        left: 700,
+      },
+    ])
+    .jpeg({ quality: 50 });
 };
